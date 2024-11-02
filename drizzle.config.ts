f8c36ff764749c92efd66a6f238ef044a18cb9e1
VladoLov <vladislav.lovric@gmail.com>
2024-11-02 18:47:17 +0100

    update neon db

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
+console.log("Database URL:", process.env.DATABASE_URL);
